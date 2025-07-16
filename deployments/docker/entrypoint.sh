ba2ddc045c825335e361d3bd1925077883cc1930
Haisam Ido <haisam.ido@nasa.gov>
2025-07-16 09:29:30 -0400

    further enhancements

--- deployments/docker/entrypoint.sh
+++ deployments/docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pkill -9 java
-rm -rf ${YAMCS_DATA_DIR}/_global.rdb/LOCK
-rm -rf ${YAMCS_DATA_DIR}/nos3.rdb/LOCK
+# pkill -9 java
+# rm -rf ${YAMCS_DATA_DIR}/_global.rdb/LOCK
+# rm -rf ${YAMCS_DATA_DIR}/nos3.rdb/LOCK
 
-mvn ${MAVEN_HTTPS_PROXY} -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DCOMPONENT_DIR=${COMPONENT_DIR} yamcs:run
+# mvn ${MAVEN_HTTPS_PROXY} -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DCOMPONENT_DIR=${COMPONENT_DIR} yamcs:run
 
 tail -f /dev/null
